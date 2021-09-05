269d219e39177e2836dd5765ec56963a2a0567ee
Peter Patocka <ppatocka@gohealth.com>
2021-09-05 17:06:55 +0200

    Show event date with Google Calendar url

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+describe('App', () => {
+    test('should render with date', () => {
+        const { getByText } = render(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+        expect(getByText(/22. May 2022/i)).toBeInTheDocument();
+    });
 });
