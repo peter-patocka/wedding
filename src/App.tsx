ceb19f36723b0cc1f5fd6430296203e4ce2c2000
Peter Patocka <ppatocka@gohealth.com>
2021-09-05 07:50:45 +0200

    Show wedding tables with guest arrangement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Map} from './app/components/Map'
+import { Map } from './app/components/Map'
 import './App.css';
-import {Header} from './app/components/Header';
-import {PhotoGallery} from './app/components/PhotoGallery';
+import { Header } from './app/components/Header';
+import { PhotoGallery } from './app/components/PhotoGallery';
 import harmonia from './assets/harmonia.jpg';
+import { WeddingTables } from './app/components/WeddingTables';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Header/>
         <img src={harmonia} alt="Eventová loď Harmónia" className="full-size" />
         <Map/>
-        <PhotoGallery/>
+        <WeddingTables />
+        <PhotoGallery />
     </div>
   );
 }
