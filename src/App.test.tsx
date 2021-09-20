77c16b27cc5c579b482b3a1ae6c407c22cf4b059
Peter Patocka <ppatocka@gohealth.com>
2021-09-20 20:06:42 +0200

    remove non-project code from create-react-app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {render} from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
     test('should render with date', () => {
-        const { getByText } = render(
-            <Provider store={store}>
-                <App />
-            </Provider>
-        );
+        const { getByText } = render(<App />);
 
         expect(getByText(/22. May 2022/i)).toBeInTheDocument();
     });
