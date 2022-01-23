e67249f35dca062abca26e4ec7c246cd1dc71ee7
Peter Patocka <ppatocka@gohealth.com>
2022-01-23 08:53:27 +0100

    accept invitation code

--- api/api.php
+++ api/api.php
@@ -3,20 +3,23 @@
 include "configuration.php";
 
 header('Access-Control-Allow-Origin: *');
- 
+
 // include "authenticate.php"; // CORS Credential is not supported if the CORS header 'Access-Control-Allow-Origin' is '*'
 
 header("Content-type: application/json; charset=utf-8");
 
 $fn = $_GET['fn'];
 
-switch($_GET['fn']) {
+switch(strtok($_GET['fn'], '/')) {
     case 'learnhunter':
         include "components/learnhunter.php";
         break;
    case 'wedding-tables':
            include "components/wedding-tables.php";
            break;
+    case 'invitation':
+        include "components/invitation.php";
+        break;
     default:
         echo '{"status":"up"}';
 }
