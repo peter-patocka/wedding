7e275bce3f17fec9068140b1c6d22c6ffa9c0b2c
Peter Patocka <ppatocka@gohealth.com>
2022-01-26 17:44:21 +0100

    move libraries & configurations to subfolder (refactor)

---
+++ api/include/configuration.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR);
+
+define("MYSQL_SERVER", "mysql");
+define("MYSQL_USERNAME", "username");
+define("MYSQL_PASSWORD", "password");
+define("MYSQL_DBNAME", "dbname");
+?>
