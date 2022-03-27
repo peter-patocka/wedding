c1a825b584cc65a68b9ff36c25ca76aebb9d08bc
Peter Patocka <ppatocka@gohealth.com>
2022-03-27 20:55:46 +0200

    add react-router-redux

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,26 +5,12 @@ import harmonia from "../assets/harmonia.jpg";
 import { Map } from "../app/components/Map";
 import { WeddingTables } from "../app/components/WeddingTables";
 import { PhotoGallery } from "../app/components/PhotoGallery";
-import { Guest } from "../types/entity/Guest";
-import { onFetchGuestsSuccess } from "../store/guests/epics";
-import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
-
-    const navigate = useNavigate();
-
-    const onUpdate = (rows: Guest[]) => {
-        alert('Thanks for your attendance!');
-        if(rows.length > 0) {
-            onFetchGuestsSuccess(rows);
-            navigate("/invitation/"+ rows[0].invitation_code);
-        }
-    };
-
     return (
         <>
             <Header/>
-            <InvitationForm onUpdate={onUpdate}/>
+            <InvitationForm/>
             <img src={harmonia} alt="Eventová loď Harmónia" className="full-size"/>
             <Map/>
             <WeddingTables/>
