f7cfa7d82d363b2612d72eff8fca9a5d5d563dd7
Peter Patocka <ppatocka@gohealth.com>
2022-04-10 20:05:35 +0200

    increment version to 1.1.0

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import { Header } from "../app/components/Header";
 import { InvitationForm } from "../app/components/InvitationForm";
 import harmonia from "../assets/harmonia.jpg";
 import { Map } from "../app/components/Map";
-import { WeddingTables } from "../app/components/WeddingTables";
 import { PhotoGallery } from "../app/components/PhotoGallery";
 import { Countdown } from "../app/components/Countdown";
 
@@ -15,7 +14,6 @@ export const HomePage = () => {
             <InvitationForm/>
             <img src={harmonia} alt="Eventová loď Harmónia" className="full-size"/>
             <Map/>
-            <WeddingTables/>
             <PhotoGallery/>
         </>
     );
