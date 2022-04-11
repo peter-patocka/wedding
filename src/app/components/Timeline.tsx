217e09b41bf7f804e03fc62dc1ec2368bf714a12
Peter Patocka <ppatocka@gohealth.com>
2022-04-11 20:51:35 +0200

    enable timeline component on guest page

--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -8,50 +8,57 @@ interface TimelineBox {
     time: string;
     description?: string;
 }
+const boxes: TimelineBox[] = [
+    {
+        id: 'timeline-10',
+        title: 'Otvorenie móla',
+        time: '10:00',
+        description: 'Vstup na ľoď je možný od 10:00. V lodnom bufete bude možné sa ponúknuť kávou a nápojmi.'
+    },
+    {
+        id: 'timeline-11',
+        title: 'Obrad',
+        time: '11:00',
+        description: 'Obrad sa bude konať na hornej palube. ' +
+            'Pre prípad nepriaznivého počasie si nezabudnite doniesť pršiplášťe alebo dáždnik 😎.'
+    },
+    {
+        id: 'timeline-12',
+        title: 'Obed',
+        time: '12:00',
+        description: 'Prípadné intolerancie nám môžete uviesť do formulára vyššie.',
+    },
+    {
+        id: 'timeline-13',
+        title: 'Organizovaný spoločný program',
+        time: '13:00',
+    },
+    {
+        id: 'timeline-15',
+        title: 'Krájanie torty',
+        time: '15:00'
+    },
+    {
+        id: 'timeline-16',
+        title: 'Voľný program',
+        time: '16:00',
+        description: 'K dispozícií bude pokrový stôl, workshop maľovania, animátorka pre deti a samozrejme tanečná zábava v podpalubí.',
+    },
+    {
+        id: 'timeline-18',
+        title: 'Večera',
+        time: '18:00',
+        description: 'Večera bude formou bufetových stolov.',
+    },
+    {
+        id: 'timeline-21',
+        title: 'Ukončenie akcie',
+        time: '21:00',
+        description: 'Ľoď máme prenajatú do 22:00.'
+    }
+]
 
 export const Timeline = () => {
-    const queryParams = new URLSearchParams(window.location.search);
-    if(queryParams.get("timeline") != '1') {
-        return (
-          <></>
-        );
-    }
-
-    const boxes: TimelineBox[] = [
-        {
-            id: 'timeline-10',
-            title: 'Otvorenie móla',
-            time: '10:00',
-            description: 'Vstup na ľoď je možný od 10:00. V lodnom bufete bude možné sa ponúknuť kávou a nápojmi.'
-        },
-        {
-            id: 'timeline-11',
-            title: 'Obrad',
-            time: '11:00',
-            description: 'Obrad sa bude v prípade priaznivého počasia konať na hornej palube'
-        },
-        {
-            id: 'timeline-12',
-            title: 'Obed',
-            time: '12:00'
-        },
-        {
-            id: 'timeline-13',
-            title: 'Hlavný program',
-            time: '13:00 - 15:00'
-        },
-        {
-            id: 'timeline-15',
-            title: 'Krájanie torty',
-            time: '15:00'
-        },
-        {
-            id: 'timeline-21',
-            title: 'Ukončenie akcie',
-            time: '21:00'
-        }
-    ]
-
     return (
         <div className="timeline-component component" id="fh5co-couple-story">
             <div className="container">
