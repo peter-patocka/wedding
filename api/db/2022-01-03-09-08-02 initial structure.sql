c810b25077fcb3054630c9aa540a496d1799fb2f
Peter Patocka <ppatocka@gohealth.com>
2022-05-14 18:14:46 +0200

    Decommission learnhunter api

--- api/db/2022-01-03-09-08-02 initial structure.sql
+++ api/db/2022-01-03-09-08-02 initial structure.sql
@@ -2,25 +2,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 SET NAMES 'utf8';
 
--- learn hunter
-CREATE TABLE `learnhunter` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `learnhunter` (`id`, `name`, `email`, `created_at`) VALUES
-(1, 'sobuz', 'sobuz@gmail.com', '2019-01-30 08:00:00'),
-(2, 'mmr', 'mmr@gmail.com', '2019-01-30 08:00:00');
-
-ALTER TABLE `learnhunter`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `learnhunter`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
--- wedding tables
 CREATE TABLE `wedding-table` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
